861940219963e5713f47e2bfc3f2cd34d132fcfb
claudiaelenadvd <31105127+claudiaelenadvd@users.noreply.github.com>
2017-08-17 10:46:42 -0500

    ComentarioCEDVD_HolaMundo

--- HolaMundo.c
+++ HolaMundo.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
 int main(){
-  printf("\nHola Mundo");
+  printf("\n¡Hola, mundo! :)");
+  printf("\nEstoy en GitHub pasándola boooomba");
+  printf("\nClaudia Elena del Valle Domínguez");
   return 0;
 }
